defab5600b4e669ba729af9844b80150a2138339
i-radi <ibrahimmohammedradi@gmail.com>
2023-10-29 14:13:55 +0200

    fix typo

--- 01-Software Engineering/01-05-Algorithms/Algorithms/Algorithms/Controllers/AlgorithmsController.cs
+++ 01-Software Engineering/01-05-Algorithms/Algorithms/Algorithms/Controllers/AlgorithmsController.cs
@@ -74,7 +74,7 @@ namespace Algorithms.Controllers
 
         [HttpGet("Huffman'sAlgorithm")]
         [SwaggerOperation(description: "The output from Huffman's algorithm can be viewed as a variable length code table for encoding a source symbol. The algorithm derives this table from the estimated probability or frequency of occurrence for each possible value of the source symbol. as in other entropy encoding methods, more common symbols are generally represented using fewer bits than less common symbols. Huffman's method can be efficiently implemented, finding a code in time linear to the number of input weights if these weights are sorted. However, although optimal among methods encoding symbols separately, Huffman coding is not always optimal among all compression methods it is replaced with arithmetic coding or asymmetric numeral systems if better compression ratio is required.")]
-        public IActionResult sAlgorithm([FromQuery] string msg)
+        public IActionResult HuffmansAlgorithm([FromQuery] string msg)
         {
             Huffman huff = new Huffman(msg);
             return Ok(huff.codes);
